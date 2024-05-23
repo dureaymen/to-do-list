18d176d09bc28a7c50fcb6e617cf92ab02c01835
dureaymen <dureaymen@gmail.com>
2024-05-23 15:23:54 +0500

    push my todo list on github

---
+++ index.ts
@@ -0,0 +1,27 @@
+#! usr/bin/env node
+
+import inquirer from "inquirer";
+
+let todoList = [];
+let condition = true
+
+while(condition){
+let addTask = await inquirer.prompt(
+    [
+        {
+            name: "todo",
+            message: "what you want to add in your todolist?",
+            type: "input",
+        },
+        {
+            name: "addMore",
+            message: "are you sure to want to add more in your todoList",
+            type: "confirm",
+            default: "false",
+        }
+    ]
+);
+todoList.push(addTask.todo);
+condition = addTask.addMore
+console.log(todoList);
+};
